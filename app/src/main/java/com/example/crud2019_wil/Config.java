898cff6df104ada9c3197930a719d29c30f89f9e
wilfredo119 <cubiasmontes11@gmail.com>
2019-11-08 13:46:58 -0600

    se agrego el acerca de

--- app/src/main/java/com/example/crud2019_wil/Config.java
+++ app/src/main/java/com/example/crud2019_wil/Config.java
@@ -2,8 +2,8 @@ package com.example.crud2019_wil;
 
 public class Config {
 
-    /*
-    URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
+
+    //URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
     public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
     public static final String urlActualizar = "http://mjgl.com.sv/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
@@ -14,21 +14,21 @@ public class Config {
     public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-      */
 
+/*
     //URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.
     //public static final String urlGuardar = "http://192.168.43.46/mysqlcrud/guardar.php";                               //Guardar datos DB.
-     public static final String urlGuardar = "http://192.168.0.102/mysqlcrud/guardar.php";                               //Guardar datos DB.
+     public static final String urlGuardar = "http://192.168.43.213/mysqlcrud/guardar.php";                               //Guardar datos DB.
 
-    public static final String urlEliminar = "http://192.168.0.102/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
-    public static final String urlActualizar = "http://192.168.0.102/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
+    public static final String urlEliminar = "http://192.168.43.213/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
+    public static final String urlActualizar = "http://192.168.43.213/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
 
-    public static final String urlConsultaApiMySQLi = "http://192.168.0.102/mysqlcrud/Api.php";                          //Ver todos los registros DB.
-    public static final String urlConsultaApiPDO = "http://192.168.0.102/mysqlcrud/buscarAll.php";                       //Ver todos los registros DB.
+    public static final String urlConsultaApiMySQLi = "http://192.168.43.213/mysqlcrud/Api.php";                          //Ver todos los registros DB.
+    public static final String urlConsultaApiPDO = "http://192.168.43.213/mysqlcrud/buscarAll.php";                       //Ver todos los registros DB.
 
-    public static final String urlConsultaCodigo = "http://192.168.0.102/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
-    public static final String urlConsultaDescripcion = "http://192.168.0.102/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
-    public static final String urlConsultaAllArticulos = "http://192.168.0.102/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-
+    public static final String urlConsultaCodigo = "http://192.168.43.213/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
+    public static final String urlConsultaDescripcion = "http://192.168.43.213/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
+    public static final String urlConsultaAllArticulos = "http://192.168.43.213/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
+*/
 
 }
