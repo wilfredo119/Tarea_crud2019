536d134b3f4c56d84341fbb1ee215c1eb8ea6a9b
wilfredo119 <57268805+wilfredo119@users.noreply.github.com>
2019-11-07 22:56:47 -0600

    primer commit

---
+++ app/src/main/java/com/example/crud2019_wil/Dto_Login.java
@@ -0,0 +1,5 @@
+package com.example.crud2019_wil;
+
+public class Dto_Login {
+
+}
---
+++ app/src/main/java/com/example/crud2019_wil/Login.java
@@ -0,0 +1,14 @@
+package com.example.crud2019_wil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
